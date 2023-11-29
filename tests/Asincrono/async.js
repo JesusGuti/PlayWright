9ee836bfb2df99aca81fc99f41eccbcc4c6b3df1
JesusGuti <jesusgonzales0968@gmail.com>
2023-11-28 20:39:59 -0400

    promise: then/catch

--- tests/Asincrono/async.js
+++ tests/Asincrono/async.js
@@ -1,4 +1,4 @@
-// Sincrono
+/* // Sincrono
 console.log("-----------------sincrono-------------")
 console.log("Inicio")
 console.log(1)
@@ -9,10 +9,31 @@ console.log("fin")
 // Asíncrono : Mecanismo de ejecucion no secuencial de JS
 console.log("-----------------asincrono-------------")
 console.log("Inicio")
-     setTimeout(function(){
-          console.log(1)
-     },2000)
-     setTimeout(function(){
-          console.log(2)
-     },1000)
-console.log("fin")
+setTimeout(function () {
+     console.log(1)
+}, 2000)
+setTimeout(function () {
+     console.log(2)
+}, 1000)
+console.log("fin") */
+
+//Promesas
+// Estado pendiente | resuelto | error
+const promesa = new Promise(function(resolve,reject){
+     //const seCumplio = fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+     const seCumplio = false
+     if(seCumplio){
+          //Resolve devuelve un valor
+          resolve("salio todo correcto")
+     }else{
+
+          reject("salio mal")
+     }     
+})
+
+/**Es como un try catch**/
+promesa.then(function(value){
+     console.log(value)
+}).catch(function(reason){
+     console.log(reason)
+})
